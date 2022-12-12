06edeab7a297c7cd4a93b1d9bc1879f1cf928983
AvishkarMandlik <mandlikavi121@gmail.com>
2022-12-12 13:51:33 +0530

    navbar added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import "./Home.css"
-
+import Navbar from "../../components/Navbar/Navbar"
 function Home() {
   return (
     <div>
+      <Navbar />
         <h1>Home</h1>
     </div>
   )
