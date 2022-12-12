67c391b62b2e97a986bef1fdee33bccb4eda2b42
AvishkarMandlik <mandlikavi121@gmail.com>
2022-12-12 14:11:55 +0530

    navbar setup

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,12 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
     <div>
       <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Navbar
-          </a>
+          <Link class="navbar-brand" href="#">📝 tiny-notes</Link>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
             aria-controls="navbarNav"
             aria-expanded="false"
@@ -17,9 +16,10 @@ function Navbar() {
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
+                <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link active" aria-current="page" to="/add-note">Add Note</Link>
               </li>
             </ul>
           </div>
