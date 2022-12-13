96e5b610c6e27830e6a1123f9c3022fe0789bfae
AvishkarMandlik <mandlikavi121@gmail.com>
2022-12-13 17:03:39 +0530

    feat: add note

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -7,30 +7,27 @@ import Note from "./../../components/Note/Note";
 function Home() {
   const [notes, setNotes] = useState([
     {
-      title: "Note 1",
-      content: "This is conntent of note 1",
+      title: "Note",
+      content: "You Can Add More Note To This List.",
     },
-    {
-      title: "Note 2",
-      content: "This is conntent of note 1",
-    },
-    {
-      title: "Note 3",
-      content: "This is conntent of note 1",
-    },
-    {
-      title: "Note 4",
-      content: "This is conntent of note 1",
-    },
-    {
-      title: "Note 5",
-      content: "This is conntent of note 1",
-    }
   ]);
 
   const [title, setTitle] = useState("")
   const [content, setContent] = useState("")
 
+
+  function addNote(){
+    const newNote = {
+      "title":title,
+      "content":content
+    }
+    setNotes([...notes, newNote])
+
+    alert('Note added successfully')
+
+    setTitle("")
+    setContent("")
+  }
   return (
     <div>
       <div className="app-title-container">
@@ -49,13 +46,13 @@ function Home() {
             <h3 className="text-center">Add Notes</h3>
             <form>
               <div>
-                <input type="text" className="form-control mt-4" placeholder="Note title" />
+                <input type="text" onChange={(e)=>{setTitle(e.target.value)}} value={title} className="form-control mt-4" placeholder="Note title" />
               </div>
               <div>
-                <input type="text" className="form-control mt-4" placeholder="Note Description" />
+                <input type="text" onChange={(e)=>{setContent(e.target.value)}} value={content} className="form-control mt-4" placeholder="Note Description" />
               </div>
               <div className="text-center">
-                <button type="button" className="btn btn-primary mt-4">Add Note</button>
+                <button type="button" onClick={addNote} className="btn btn-primary mt-4">Add Note</button>
               </div>
             </form>
           </div>
